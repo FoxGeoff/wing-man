5cdce31ee2dcec78d0d3b5ca22d675f602da00cc
Geoff Fox <fox.geoffrey@gmail.com>
2021-02-24 15:28:01 -0800

    Account-detail Component

--- src/app/account/account-details/account-details.component.ts
+++ src/app/account/account-details/account-details.component.ts
@@ -1,11 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
 @Component({
   selector: 'app-account-details',
   templateUrl: './account-details.component.html',
   styleUrls: ['./account-details.component.css']
 })
 export class AccountDetailsComponent implements OnInit {
+
+  /**
+   * @title List with sections
+   */
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  notes: Section[] = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
 
   constructor() { }
 
