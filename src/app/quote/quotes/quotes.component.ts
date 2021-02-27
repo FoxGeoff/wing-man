52419c5a79146874a05b15e91e7798b17f39aacf
Geoff Fox <fox.geoffrey@gmail.com>
2021-02-27 11:00:33 -0800

    Add quotes list

--- src/app/quote/quotes/quotes.component.ts
+++ src/app/quote/quotes/quotes.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Quote {
+  id: number;
+  name: string;
+  price: number;
+  unit: string;
+  description: string;
+}
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
@@ -9,6 +16,30 @@ export class QuotesComponent implements OnInit {
 
   constructor() { }
 
+  quotes: Quote[] = [
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+    { id: 1, name: 'Aluminium', price: 2.99, unit: 'sq inch', description: '0.004" thick clad sheet' },
+  ];
   ngOnInit(): void {
   }
 
