70e57b84b846e60d38fd38f381fe945b9e043e0e
Geoff Fox <fox.geoffrey@gmail.com>
2021-03-02 18:08:59 -0800

    Working AccountsList (http from in-mem-db)

--- src/app/account/accounts-list/accounts-list.component.ts
+++ src/app/account/accounts-list/accounts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account.service';
+import { Account } from '../../data/interface/account';
 
 @Component({
   selector: 'app-accounts-list',
@@ -22,4 +23,39 @@ export class AccountsListComponent implements OnInit {
       );
   }
 
+  deleteAccount(id: number) {
+
+    this.accountService.deleteAccount(id).subscribe((ret) => {
+      console.log('Account deleted: ', ret);
+    })
+  }
+
+  updateAccount(account: Account) {
+
+    // because we are using interface instead of class
+    // https://stackoverflow.com/questions/13142635/how-can-i-create-an-object-based-on-an-interface-file-definition-in-typescript
+    // If you want an empty object of an interface, you can do just:
+    // tslint:disable-next-line:prefer-const
+    let anAccount = <Account>{}; // an  empty object of type Property
+
+    const newAccount: Account = {
+      id: account.id,
+      name: 'Acme Carpet Cleaning',
+      website: 'acme.com',
+      description: 'A local carpet cleaning service',
+      street: '456 Main St.',
+      city: 'San Diego',
+      state: 'CA',
+      zip: 93125,
+      country: 'USA',
+      taxId: 'T124-456-7777',
+      paymentTerms: '30Net',
+      email: 'sales@acme-carpet.com',
+      phone: 619333475
+    }
+
+    this.accountService.updateAccount(newAccount).subscribe((ret) => {
+      console.log('Account updated: ', ret);
+    });
+  }
 }
