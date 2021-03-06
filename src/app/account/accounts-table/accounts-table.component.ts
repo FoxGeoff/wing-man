fc22af20126ea37fe9e03133849c9420ae3f5fc3
Geoff Fox <fox.geoffrey@gmail.com>
2021-03-06 03:58:48 -0800

    tidy up

--- src/app/account/accounts-table/accounts-table.component.ts
+++ src/app/account/accounts-table/accounts-table.component.ts
@@ -2,9 +2,7 @@ import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core'
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-// chk box section
 import {SelectionModel} from '@angular/cdk/collections';
-
 import { Account } from '../../data/Models/account';
 
 /**
