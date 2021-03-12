a9b67c59675c8c3c0100471ca5321f5c0f0d7527
Geoff Fox <fox.geoffrey@gmail.com>
2021-03-12 11:58:51 -0800

    tidy up

--- src/app/contact/add-contact/add-contact.component.ts
+++ src/app/contact/add-contact/add-contact.component.ts
@@ -7,11 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AddContactComponent implements OnInit {
 
-  constructor(private router: Router) {
-    this.router.routeReuseStrategy.shouldReuseRoute = () => {
-      return false;
-    };
-   }
+  constructor(private router: Router) { }
 
 
   ngOnInit(): void {
